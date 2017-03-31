d5b7b3cf0fa94bcdda8e6060de389902d323fec0
Robin Bolterman <rbolterman@hotmail.com>
2017-03-31 10:45:55 -0400

    Delete listings.component.ts

--- listings/listings.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    selector: 'listings-home-page',
-    templateUrl: './app/components/listings/listings.component.html'
-})
-export class ListingsComponent {
-    //Was ClaimsComponent
-}
