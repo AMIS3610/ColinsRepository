efa8f70c9c966f850c736293dcd4c8393fe9a388
Robin Bolterman <rbolterman@hotmail.com>
2017-03-31 10:32:04 -0400

    Add files via upload

---
+++ wwwroot/App/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+      selector: "fisher-insurance",
+      templateUrl: './App/app.component.html'
+})
+
+export class AppComponent { }
+
+
+///Instance of Fisher-insurance, change later
